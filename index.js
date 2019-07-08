8e59b13927c8aa1fce3c28d76043a41468729e46
raksa <eng.raksa@gmail.com>
2019-07-08 11:14:07 +0700

    add connection testing

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const sequelize = require('./sequelize');
+
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+}).catch(err => {
+    console.error('Unable to connect to the database:', err);
+});
